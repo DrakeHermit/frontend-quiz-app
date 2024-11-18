bfce71dd9e6f9bba07785af945d04831c99e409e
Jan Kotvaš <jan.kotvas96@gmail.com>
2024-11-18 19:50:04 +0100

    Added data attributes for specific categories plus started fetching data from the backend

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,2 +1,14 @@
 import themeSwitcher from "./themeSwitcher.js";
 themeSwitcher();
+
+const buttons = document.querySelectorAll(".button__group");
+
+const fetchQuestionsByCategory = async (category) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/questions/${category}`);
+    const questions = res.json();
+    return questions;
+  } catch (error) {
+    console.log("Failed to fetch the data");
+  }
+};
