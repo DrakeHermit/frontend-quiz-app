3d62caf6ce329cc9087eaaef9ea91c39ad30dea0
Jan Kotvaš <jan.kotvas96@gmail.com>
2024-11-24 18:28:54 +0100

    Started working on the running quiz state by showing the question and the answers

--- frontend/js/quiz.js
+++ frontend/js/quiz.js
@@ -23,6 +23,32 @@ export class Quiz {
   async handleCategoryClick(e) {
     const category = e.currentTarget.dataset.category;
     this.questions = await this.service.fetchQuestions(category);
-    console.log(this.questions);
+    this.displayRandomQuestion();
+  }
+
+  displayRandomQuestion() {
+    const randomIndex = Math.floor(Math.random() * this.questions.length);
+    const question = this.questions[randomIndex];
+
+    // Selectors
+    const title = document.querySelector(".main__heading");
+    title.textContent = question.question;
+    title.style.fontSize = "36px";
+    const progress = document.getElementById("remove");
+    progress.innerHTML = "";
+    const answerText = document.querySelectorAll(".button__group span");
+    answerText.forEach((answer, index) => {
+      answer.textContent = question.options[index];
+    });
+    const answerChoice = document.querySelectorAll(".img-wrapper");
+    const letters = ["A", "B", "C", "D"];
+    answerChoice.forEach(
+      (choice, index) =>
+        (choice.innerHTML = `
+        <div class="img-wrapper bold-big">
+          ${letters[index]}
+        </div> 
+      `)
+    );
   }
 }
