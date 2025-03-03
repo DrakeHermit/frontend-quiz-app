51ff8b47ae21a0dc090e107a2ba2244c625f93d8
Jan Kotvaš <jan.kotvas96@gmail.com>
2025-03-03 15:35:04 +0100

    fix: fixed the path issue in my theme switcher function

--- frontend/js/themeSwitcher.js
+++ frontend/js/themeSwitcher.js
@@ -8,27 +8,23 @@ const movableCirc = document.querySelector(".movable");
 const toggleBtn = document.querySelector(".toggler");
 
 export default function themeSwitcher() {
-  document.querySelector(".toggler").addEventListener("click", () => {
+  toggleBtn.addEventListener("click", () => {
     movableCirc.classList.toggle("toggle");
     body.classList.toggle("main-light");
     if (body.classList.contains("main-light")) {
       overlayDesktop.src =
-        "/frontend/assets/images/pattern-background-desktop-light.svg";
-      overlayTablet.src =
-        "/frontend/assets/images/pattern-background-tablet-light.svg";
-      overlayMobile.src =
-        "/frontend/assets/images/pattern-background-mobile-light.svg";
-      sunIcon.src = "/frontend/assets/images/icon-sun-dark.svg";
-      moonIcon.src = "/frontend/assets/images/icon-moon-dark.svg";
+        "./assets/images/pattern-background-desktop-light.svg";
+      overlayTablet.src = "./assets/images/pattern-background-tablet-light.svg";
+      overlayMobile.src = "./assets/images/pattern-background-mobile-light.svg";
+      sunIcon.src = "./assets/images/icon-sun-dark.svg";
+      moonIcon.src = "./assets/images/icon-moon-dark.svg";
     } else {
       overlayDesktop.src =
-        "/frontend/assets/images/pattern-background-desktop-dark.svg";
-      overlayTablet.src =
-        "/frontend/assets/images/pattern-background-tablet-dark.svg";
-      overlayMobile.src =
-        "/frontend/assets/images/pattern-background-mobile-dark.svg";
-      sunIcon.src = "/frontend/assets/images/icon-sun-light.svg";
-      moonIcon.src = "/frontend/assets/images/icon-moon-light.svg";
+        "./assets/images/pattern-background-desktop-dark.svg";
+      overlayTablet.src = "./assets/images/pattern-background-tablet-dark.svg";
+      overlayMobile.src = "./assets/images/pattern-background-mobile-dark.svg";
+      sunIcon.src = "./assets/images/icon-sun-light.svg";
+      moonIcon.src = "./assets/images/icon-moon-light.svg";
     }
   });
 }
